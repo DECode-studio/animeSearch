b2684781e57e952fe107a200442c4d13ec55e2ef
user <user@users-MacBook-Pro-2.local>
2025-11-12 09:57:33 +0700

    first commit

---
+++ animeSearchTests/animeSearchTests.swift
@@ -0,0 +1,16 @@
+//
+//  animeSearchTests.swift
+//  animeSearchTests
+//
+//  Created by user on 12/11/25.
+//
+
+import Testing
+
+struct animeSearchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ animeSearchUITests/animeSearchUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  animeSearchUITestsLaunchTests.swift
+//  animeSearchUITests
+//
+//  Created by user on 12/11/25.
+//
+
+import XCTest
+
+final class animeSearchUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
